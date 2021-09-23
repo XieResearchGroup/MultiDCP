7113ab9ff147284daf9849b86ccefb06df5a71d6
AdorableYoyo <youwu9254@gmail.com>
2021-09-23 18:30:19 +0000

    small

--- script/train_multidcp_ae.sh
+++ script/train_multidcp_ae.sh
@@ -3,10 +3,10 @@
 
 
 python /raid/home/yoyowu/MultiDCP/MultiDCP/multidcp_ae.py --device 0 \
---exp '0920_binary_up_gene' \
+--exp '0922_binary_down_gene' \
 --drug_file "/raid/home/yoyowu/MultiDCP/MultiDCP_data/data/all_drugs_l1000.csv" \
---gene_file "/raid/home/yoyowu/MultiDCP/MultiDCP_data/data/gene_vector.csv"  --train_file "/raid/home/yoyowu/MultiDCP/MultiDCP_data/ranking_binary/up_signature_train_1.csv" \
---dev_file "/raid/home/yoyowu/MultiDCP/MultiDCP_data/ranking_binary/up_signature_dev_1.csv"  --test_file "/raid/home/yoyowu/MultiDCP/MultiDCP_data/ranking_binary/up_signature_test_1.csv"  \
+--gene_file "/raid/home/yoyowu/MultiDCP/MultiDCP_data/data/gene_vector.csv"  --train_file "/raid/home/yoyowu/MultiDCP/MultiDCP_data/ranking_binary/down_signature_train_1.csv" \
+--dev_file "/raid/home/yoyowu/MultiDCP/MultiDCP_data/ranking_binary/down_signature_dev_1.csv"  --test_file "/raid/home/yoyowu/MultiDCP/MultiDCP_data/ranking_binary/down_signature_test_1.csv"  \
 --dropout 0.3 --batch_size 64 --max_epoch 500 \
 --ae_input_file "/raid/home/yoyowu/MultiDCP/MultiDCP_data/data/gene_expression_for_ae/gene_expression_combat_norm_978_split4" \
 --ae_label_file "/raid/home/yoyowu/MultiDCP/MultiDCP_data/data/gene_expression_for_ae/gene_expression_combat_norm_978_split4" \
@@ -14,6 +14,6 @@ python /raid/home/yoyowu/MultiDCP/MultiDCP/multidcp_ae.py --device 0 \
 --hidden_repr_result_for_testset "/raid/home/yoyowu/MultiDCP/MultiDCP_data/data/teacher_student/teach_stu_perturbedGX_hidden.csv" \
 --cell_ge_file "/raid/home/yoyowu/MultiDCP/MultiDCP_data/data/adjusted_ccle_tcga_ad_tpm_log2.csv" \
 --all_cells "/raid/home/yoyowu/MultiDCP/MultiDCP_data//data/ccle_tcga_ad_cells.p" \
-> MultiDCP/Sep_outlogs/ranking_up_gene_binary.log 2>&1 &
+> MultiDCP/Sep_outlogs/ranking_down_gene_binary.log 2>&1 &
 echo "done"
 # > ../MultiDCP/output/cellwise_output_ran5.txt
